0b7477d5ec02396d37b619f1788d54c1e7e99040
LeAnne B <branchleannelmb@gmail.com>
2024-03-27 14:48:21 -0400

    populates circular nav

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import bg from "../../public/background/home-background.png";
 import RenderModel from "@/components/RenderModel";
 import Wizard from "@/components/models/Wizard";
 import Navigation from "@/components/navigation";
+import Witch from "@/components/models/Witch";
 import magictome from "../../website images/magictome.png";
 
 export default function Home() {
@@ -27,6 +28,7 @@ export default function Home() {
         /> */}
         <RenderModel>
           <Wizard />
+          {/* <Witch/> */}
         </RenderModel>
       </div>
     </main>
