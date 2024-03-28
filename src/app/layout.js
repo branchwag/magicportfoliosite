3d77f8ede5a731f7db888cff5f5c551f4736cc2c
LeAnne B <branchleannelmb@gmail.com>
2024-03-27 22:12:32 -0400

    adds fireflies

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import FireFliesBackground from "@/components/FireFliesBackground";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
         )}
       >
         {children}
+        <FireFliesBackground />
       </body>
     </html>
   );
